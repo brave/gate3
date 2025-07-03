d68ac3c909dc7a2e8fcc3b278b41646986cfbc32
Harold Spencer Jr. <hspencer77@gmail.com>
2025-07-02 22:13:35 -0700

    Add redis database information (#43)
    * Add Redis information
    
    * Updated accessing Redis password from environment variable REDIS_PASSWORD
    
    * Revert fix for REDIS PASSWORD

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     COINGECKO_API_KEY: str | None = None
     ALCHEMY_API_KEY: str | None = None
 
-    REDIS_HOST: str = "localhost"
+    REDIS_HOST: str = "redis-master"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
     REDIS_PASSWORD: str | None = None
