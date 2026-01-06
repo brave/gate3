e6f2c29af65b43c020f14c5c6cb2d22de2a86e85
mschfh <37435502+mschfh@users.noreply.github.com>
2026-01-06 05:38:53 -0500

    fix: query attribute check

--- app/api/oauth/zebpay.py
+++ app/api/oauth/zebpay.py
@@ -28,7 +28,7 @@ async def auth(environment: Environment, request: Request) -> RedirectResponse:
 
     # Extract the returnUrl parameter which contains another URL
     req_return_url = URL(query_params.get("returnUrl"))
-    if not req_return_url:
+    if not req_return_url.query:
         raise HTTPException(status_code=400, detail="Missing returnUrl parameter")
 
     params = parse_qs(req_return_url.query)
